5602f5d26ee0cd71dd00f0af13e6a2abdb9ea183
Sean Leavey <gpg@attackllama.com>
2020-03-11 20:46:40 +0100

    Move function into global namespace

--- labbook/functions.php
+++ labbook/functions.php
@@ -13,6 +13,24 @@ define( 'LABBOOK_VERSION', '1.1.13' );
 // Required PHP version.
 define( 'LABBOOK_MINIMUM_PHP_VERSION', '7.0.0' );
 
+if ( ! function_exists( 'labbook_version_too_low_admin_notice' ) ) :
+	/**
+	 * Notify admin that their PHP version is too low and return to the previous theme.
+	 */
+	function labbook_version_too_low_admin_notice() {
+		echo '<div class="update-nag">';
+		esc_html_e( 'Labbook cannot run on the currently installed PHP version.', 'labbook' );
+		echo '<br/>';
+		printf(
+			/* translators: 1: current PHP version, 2: required PHP version */
+			esc_html__( 'Actual version is: %1$s, required version is: %2$s.', 'labbook' ),
+			esc_html( phpversion() ),
+			esc_html( LABBOOK_MINIMUM_PHP_VERSION )
+		);
+		echo '</div>';
+	}
+endif;
+
 if ( ! function_exists( 'labbook_check_php_version' ) ) :
 	/**
 	 * Detect current PHP version and prevent theme switch if not recent enough.
@@ -23,22 +41,6 @@ if ( ! function_exists( 'labbook_check_php_version' ) ) :
 	function labbook_check_php_version( $old_name, $old_theme = null ) {
 		// Compare versions.
 		if ( version_compare( phpversion(), LABBOOK_MINIMUM_PHP_VERSION, '<' ) ) {
-			/**
-			 * Notify admin that their PHP version is too low and return to the previous theme.
-			 */
-			function labbook_version_too_low_admin_notice() {
-				echo '<div class="update-nag">';
-				esc_html_e( 'Labbook cannot run on the currently installed PHP version.', 'labbook' );
-				echo '<br/>';
-				printf(
-					/* translators: 1: current PHP version, 2: required PHP version */
-					esc_html__( 'Actual version is: %1$s, required version is: %2$s.', 'labbook' ),
-					esc_html( phpversion() ),
-					esc_html( LABBOOK_MINIMUM_PHP_VERSION )
-				);
-				echo '</div>';
-			}
-
 			// Theme not activated info message.
 			add_action( 'admin_notices', 'labbook_version_too_low_admin_notice' );
 
